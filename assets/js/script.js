8ac6bebaad21a95225e9ede67d6cc9645986b622
KW <kenneth_sl_wong@msn.com>
2022-01-19 18:06:29 -0500

    changed font to black, added shell click functions to save button and event containers

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//load schedule function
+var loadSchedule = function (){
+
+};
+
+//event container clicked - edit data
+$(".event-container").on("click",function(){
+    
+    //grab existing text if it exists
+    var text = $(this).find("p").text().trim();
+});
+
+//save button clicked - save data
+$(".saveBtn").on("click",function(){
+});
+
+//initial load
+loadSchedule();
